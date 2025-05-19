520c170aed3b740a9fc249855c41a8dbf1805e67
shaulov <shineginn@gmail.com>
2025-05-20 01:58:34 +0300

    refactor: move hooks folder

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,5 +1,5 @@
 import {useCallback, useState} from 'react';
-import { useShortcut } from '../hooks/useShortcut.ts';
+import { useShortcut } from '../../hooks/useShortcut.ts';
 import './app.css';
 
 function App() {
